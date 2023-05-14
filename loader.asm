0decbe790ad76587c5ef32286e4c48bb2eb61787
ubuntu <ubuntu@ubuntu-20.04>
2023-05-14 14:46:40 +0700

    Load kernel

--- loader.asm
+++ loader.asm
@@ -29,6 +29,26 @@ start:
 	; if zero flag is set, it means that this feature is not supported and we jump to not support.
 	jz NotSupport
 
+LoadKernel:
+    mov si, ReadPacket
+    mov word[si], 0x10
+    mov word[si+2], 100
+    mov word[si+4], 0
+    mov word[si+6], 0x1000 ; offset segment 0x1000:0 = 0x1000 * 16 + 0 = 0x10000
+    ; the boot  file resides in the first sector,
+    ; the loader file occupies the next five sectors. so we write our kernel from the 7th sector
+    ; offset 0x1000:6 = 0x1000 * 16 + 6 = 0x10006\
+    mov dword[si+8], 6 ; sector value is 0-based value, so 6 means the 7th sector
+    
+    
+    mov dword[si+0xc], 0 
+    mov dl, [DriveId]
+    mov ah, 0x42
+    int 0x13
+    ; check the carry flag. If carry flag is set after calling the read service, 
+    ; we know that loading kernel failed and we jump to read error and stop here.
+    jc ReadError
+    
 
 	mov ah, 0x13
 	mov al, 1
@@ -38,11 +58,13 @@ start:
 	mov cx, MessageLen
 	int 0x10
 
+ReadError:
 NotSupport:
 End:
 	hlt
 	jmp End
 
 DriveId: db 0
-Message: db "Long mode is supported"
+Message: db "Kernel is loaded successfully!"
 MessageLen: equ $-Message
+ReadPacket: times 16 db 0
