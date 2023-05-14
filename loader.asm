fe106ee5effc7614936eca74e1cca7f378abbc3e
ubuntu <ubuntu@ubuntu-20.04>
2023-05-14 13:14:10 +0700

    Check for long mode support

--- loader.asm
+++ loader.asm
@@ -2,18 +2,47 @@
 [ORG 0x7e00]
 
 start:
-    mov ah, 0x13
-    mov al, 1
-    mov bx, 0xa
-    xor dx, dx
-    mov bp, Message
-    mov cx, MessageLen
-    int 0x10    
+	; save DriveId for later use
+	mov [DriveId], dl
+	
+	; check to see wether it supports 0x80000001 input value
+	mov eax, 0x80000000
+	cpuid
+
+	; if the return value in eax is less than 0x80000001, means it does not support input value
+	cmp eax, 0x80000001
+	jb NotSupport
+
+	; this will return the processor features
+	; the information about long mode support is saved in edx
+	mov eax, 0x80000001 
+	cpuid ; returs the processor identification and feature information.
+
+	; we test bit 29 in edx, if it's set means that long mode is supported.
+	; otherwise long mode is not available and we jump to the label not support.
+	test edx, (1 << 29)
+	; is zero flag is set, means that the bit we checked is not set and we jump to not support
+	jz NotSupport
+
+	; check 1g page support at bit 26
+	test edx, (1 << 26)
+	; if zero flag is set, it means that this feature is not supported and we jump to not support.
+	jz NotSupport
 
 
+	mov ah, 0x13
+	mov al, 1
+	mov bx, 0xa
+	xor dx, dx
+	mov bp, Message
+	mov cx, MessageLen
+	int 0x10
+
+NotSupport:
 End:
-    hlt
-    jmp End
+	hlt
+	jmp End
 
-Message: db "Loader starts"
-MessageLen: equ $ - Message
+DriveId: db 0
+Message: db "Long mode is supported"
+MessageLen: equ $-Message
