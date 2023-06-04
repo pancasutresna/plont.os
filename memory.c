aa1fb4ec87f4b84f4346ce49abf1e50166131690
pancasutresna <panca.sutresna@gmail.com>
2023-06-04 13:55:53 +0700

    memory allocator

--- memory.c
+++ memory.c
@@ -1,8 +1,14 @@
 #include "memory.h"
 #include "print.h"
 #include "debug.h"
+#include "stddef.h"
+
+static void free_region(uint64_t v, uint64_t e);
 
 static struct FreeMemRegion free_mem_region[50];
+static struct Page free_memory;
+static uint64_t memory_end;
+extern char end;
 
 void init_memory(void)
 {
@@ -26,5 +32,54 @@ void init_memory(void)
         printk("%x %uKB %u\n", mem_map[i].address, mem_map[i].length/1024, (uint64_t)mem_map[i].type);
     }
 
-    printk("Total memory is %uMB\n", total_mem/1024/1024);
+    for (int i = 0; i < free_region_count; i++)
+    {
+        uint64_t vstart = P2V(free_mem_region[i].address);
+        uint64_t vend = vstart + free_mem_region[i].length;
+
+        if (vstart > (uint64_t)&end) {
+            free_region(vstart, vend);
+        }
+        else if(vend > (uint64_t)&end){
+            free_region((uint64_t)&end, vend);
+        }
+    }
+
+    memory_end = (uint64_t)free_memory.next + PAGE_SIZE;
+    printk("%x\n", memory_end);
+}
+
+static void free_region(uint64_t v, uint64_t e)
+{
+    for (uint64_t start = PA_UP(v); start+PAGE_SIZE <= e; start += PAGE_SIZE)
+    {
+        if (start+PAGE_SIZE <= 0xffff800000000000) {
+            kfree(start);
+        }
+    }
+}
+
+void kfree(uint64_t v)
+{
+    ASSERT(v % PAGE_SIZE == 0);
+    ASSERT(v >= (uint64_t)&end);
+    ASSERT(v+PAGE_SIZE <= 0xffff800000000000);
+
+    struct Page *page_address = (struct Page*)v;
+    page_address->next = free_memory.next;
+    free_memory.next = page_address;
+}
+
+void* kalloc(void)
+{
+    struct Page *page_address = free_memory.next;
+    if (page_address != NULL) {
+        ASSERT((uint64_t)page_address % PAGE_SIZE == 0);
+        ASSERT((uint64_t)page_address >= (uint64_t)&end);
+        ASSERT((uint64_t)page_address+PAGE_SIZE <= 0xffff800000000000);
+
+        free_memory.next = page_address->next;
+    }
+
+    return page_address;
 }
--- memory.h
+++ memory.h
@@ -14,7 +14,18 @@ struct FreeMemRegion {
     uint64_t length;
 };
 
-void init_memory(void);
+struct Page {
+    struct Page* next;
+};
 
+#define PAGE_SIZE (2*1024*1024)
+#define PA_UP(v) ((((uint64_t)v+PAGE_SIZE-1)>>21)<<21)
+#define PA_DOWN(v) (((uint64_t)v>>21)<<21)
+#define P2V(p) ((uint64_t)(p)+0xffff800000000000)
+#define V2P(v) ((uint64_t)(v)-0xffff800000000000)
+
+void init_memory(void);
+void* kalloc(void);
+void kfree(uint64_t v);
 
 #endif
